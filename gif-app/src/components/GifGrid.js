bf14cb5765be2d53923aed5c5191997ce59a6b8b
unknown <davidb@soymomo.com>
2022-01-28 23:37:55 -0500

    knowlegde: create custom hooks and add animations on the app

--- gif-app/src/components/GifGrid.js
+++ gif-app/src/components/GifGrid.js
@@ -1,19 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { getGifs } from "../Helpers/GetGifs";
+import React from "react";
+import { useFetchGifs } from "../Hooks/useFetchGifs";
+// import { getGifs } from "../Helpers/GetGifs";
 import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  // only render this function one  time,
-  useEffect(() => {
-    getGifs(category).then(imgs => setImages(imgs));
-  }, [category]);
+  // const [images, setImages] = useState([]);
+ const {data:images,loading} =  useFetchGifs(category);
+ 
+  // // only render this function one  time,
+  // useEffect(() => {
+  //   getGifs(category).then(imgs => setImages(imgs));
+  // }, [category]);
 
   
 
   return (
     <>
+    <h3 className="card animate__animated animate__swing">{category}</h3>
+
+    {
+      loading && <p className="card animate__animated animate__backInLeft">Loading....</p>
+    }
+ 
       <div className="card-grid">
         <ol>
           {images.map((img) => {
@@ -21,7 +29,6 @@ export const GifGrid = ({ category }) => {
           })}
         </ol>
       </div>
-      ;
     </>
   );
 };
